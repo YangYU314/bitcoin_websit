fb78c3124e95955652395eaad68b8d4b45dcbfb5
shwu6127 <shwu6127@uni.sydney.edu.au>
2018-09-05 10:57:14 +1000

    candle stick back-end

--- controllers/home.js
+++ controllers/home.js
@@ -1,23 +1,26 @@
-const gdax = require('gdax');
-const publicClient = new gdax.PublicClient();
+var candleStickModel = require('../models/candle_stick');
 
 module.exports.showMainPage = function(req, res){
     var sess = req.session;
     if ("username" in sess && sess.username != null){
         res.render('home', {user: sess.username});
     }else{
-        res.render('home', {user: "shenghui wu"});
+        res.render('home', {user: "Shenghui wu"});
         //res.render('login');
     }
 }
 
-var temp = new Array();
-publicClient.getProductHistoricRates('BTC-USD', function(err, res, data){
-    for (var i = 0; i < 10; i++){
-        temp[i] = data[i];
-    }
-})
-
-module.exports.showData = function(req, res){
-    res.json({result: temp});
+module.exports.candle_stick = function(req, res){
+    var product_id = req.body.product_id;
+    candleStickModel.candle_stick_data(product_id, function(result){
+        res.json(result);
+    });
+    //bitcoin.candle_stick()
+    /*var temp = new Array();
+    publicClient.getProductHistoricRates('BTC-USD', function(err, res, data){
+        for (var i = 0; i < 10; i++){
+            temp[i] = data[i];
+        }
+    });
+    res.json({result: temp});*/
 }
---
+++ models/candle_stick.js
@@ -0,0 +1,106 @@
+var mongoose = require('./database');
+var Schema = mongoose.Schema;
+const gdax = require('gdax');
+const publicClient = new gdax.PublicClient();
+
+var candleStickSchema = new Schema({
+        product_id: String,
+        time: Number,
+        open: Number,
+        close: Number,
+        low: Number,
+        high: Number,
+        volume: Number
+    },
+    {
+        collection:'candle_stick'
+});
+
+//update and get data from collection candle_stick
+candleStickSchema.statics.candle_stick_data = function (product_id, callback){
+    //update data
+    Candle_stick.findNewest(product_id, function(result){
+        if(result == null){
+            publicClient.getProductHistoricRates(product_id, function (err, res, data){
+                for (var i in data){
+                    var newData = new Candle_stick({
+                        product_id: product_id,
+                        time: data[i][0],
+                        open: data[i][3],
+                        close: data[i][4],
+                        low: data[i][1],
+                        high: data[i][2],
+                        volume: data[i][5]
+                    });
+                    newData.save();
+                }
+            });
+        }else{
+            publicClient.getProductHistoricRates(product_id, function (err, res, data){
+                for (var i in data){
+                    if (data[i][0] <= result){
+                        break;
+                    }else{
+                        var newData = new Candle_stick({
+                            product_id: product_id,
+                            time: data[i][0],
+                            open: data[i][3],
+                            close: data[i][4],
+                            low: data[i][1],
+                            high: data[i][2],
+                            volume: data[i][5]
+                        });
+                        newData.save();
+                    }
+                }
+            });
+        }
+    });
+
+    //get data
+    Candle_stick.findData(product_id, function(result){
+        callback(result);
+    });
+}
+
+//find the newest row and get its time
+candleStickSchema.statics.findNewest = function (product_id, callback) {
+    var newest = [
+        {$match: {product_id: product_id}},
+        {$sort: {'time': -1}},
+        {'$limit': 1}
+    ];
+    this.aggregate(newest, function(err, data){
+        if(err){
+            console.log("Query: findNewest Error!");
+        }else{
+            if(data.length > 0){
+                callback(data[0].time);
+            }else{
+                callback(null);
+            }
+        }
+    });
+};
+
+//find all the data and get its time, open, close, low, high
+candleStickSchema.statics.findData = function (product_id, callback) {
+    var data = [
+        {$match: {product_id: product_id}},
+        {$sort: {'time': -1}}
+    ];
+    this.aggregate(data, function(err, data){
+        if(err){
+            console.log("Query: findData Error!");
+        }else{
+            if(data.length > 0){
+                callback(data);
+            }else{
+                callback(null);
+            }
+        }
+    });
+};
+
+var Candle_stick = mongoose.model('Candle_stick', candleStickSchema, 'candle_stick');
+module.exports = Candle_stick;
