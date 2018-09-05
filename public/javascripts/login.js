d524e87acf4bfea031dcea77152a49e1667b7803
Xiatong Li <xili0330@uni.sydney.edu.au>
2018-09-05 10:28:55 +1000

    update landing page & home page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -21,3 +21,65 @@ $(document).ready(function(){
         })
     })
 })
+
+function g(id){
+    return document.getElementById(id);
+}
+
+function autoCenter( el ){
+    var bodyW = document.documentElement.clientWidth;
+    var bodyH = document.documentElement.clientHeight;
+
+    var elW = el.offsetWidth;
+    var elH = el.offsetHeight;
+
+    el.style.left = (bodyW-elW)/2 + 'px';
+    el.style.top = (bodyH-elH)/2 + 'px';
+
+}
+
+
+function fillToBody( el ){
+    el.style.width  = document.documentElement.clientWidth  +'px';
+    el.style.height = document.documentElement.clientHeight + 'px';
+}
+
+document.onmouseup = function(e){
+
+    dialogInstace = false;
+    clearInterval(onMoveStartId);
+
+}
+
+
+document.onmousemove = function(e) {
+    var e = e || window.event;
+    var instace = dialogInstace;
+    if (instace) {
+
+        var maxX = document.documentElement.clientWidth -  instace.moveElement.offsetWidth;
+        var maxY = document.documentElement.clientHeight - instace.moveElement.offsetHeight ;
+
+        instace.moveElement.style.left = Math.min( Math.max( ( e.pageX - instace.mouseOffsetLeft) , 0 ) , maxX) + "px";
+        instace.moveElement.style.top  = Math.min( Math.max( ( e.pageY - instace.mouseOffsetTop ) , 0 ) , maxY) + "px";
+    }
+    if(e.stopPropagation) {
+        e.stopPropagation();
+    } else {
+        e.cancelBubble = true;
+    }
+};
+
+function showDialog(){
+    g('dialogMove').style.display = 'block';
+    g('mask').style.display = 'block';
+    autoCenter( g('dialogMove') );
+    fillToBody( g('mask') );
+}
+
+
+function hideDialog(){
+    g('dialogMove').style.display = 'none';
+    g('mask').style.display = 'none';
+}
+
