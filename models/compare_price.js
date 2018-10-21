3656ce0cf9ae4f64cf4d8b7478711db02d1eaf14
shwu6127 <shwu6127@uni.sydney.edu.au>
2018-10-21 17:02:59 +1100

    fix bugs: compare price

--- models/compare_price.js
+++ models/compare_price.js
@@ -18,29 +18,28 @@ comparePriceSchema.statics.compare_price_data = function(callback){
         if (err){
             console.log("error");
         }else{
-            var list;
             for (const key in result) {
-                if (key == 0 || key == 4 || key == 7 || key == 8){
-                    list = result[key];
+                Compare_price.find_timestamp(result[key].timestamp, function(res){
+                    if (res != null) {
+                        //save in database
+                        var newComparePrice = new Compare_price({
+                            exchange: result[key].exchange,
+                            quote: result[key].quote,
+                            price: result[key].price,
+                            timestamp: result[key].timestamp
+                        });
 
-                    //save in database
-                    var newComparePrice = new Compare_price({
-                        exchange: list.exchange,
-                        quote: list.quote,
-                        price: list.price,
-                        timestamp: list.timestamp,
-                    });
+                        newComparePrice.save();
 
-                    newComparePrice.save();
-
-                    /*//save jason into the array
-                    var mapArray = {};
-                    mapArray["exchange"] = list.exchange;
-                    mapArray["quote"] = list.quote;
-                    mapArray["price"] = list.price;
-                    mapArray["timestamp"] = list.timestamp;
-                    temp.push(mapArray);*/
-                }
+                        /*//save jason into the array
+                        var mapArray = {};
+                        mapArray["exchange"] = list.exchange;
+                        mapArray["quote"] = list.quote;
+                        mapArray["price"] = list.price;
+                        mapArray["timestamp"] = list.timestamp;
+                        temp.push(mapArray);*/
+                    }
+                });
             }
 
             Compare_price.findAllData(function(result){
@@ -50,10 +49,42 @@ comparePriceSchema.statics.compare_price_data = function(callback){
     });
 }
 
+//function to get data from URL
+function getData(url, callback){
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            callback(error, body);
+        }
+    });
+}
+
+//find if the timestamp exists
+comparePriceSchema.statics.find_timestamp = function(timestamp, callback){
+    var data = [
+        {$match: {timestamp: timestamp}},
+        {$limit: 1}
+    ];
+
+    this.aggregate(data, function(err, data){
+        if(err){
+            console.log("Query: find_timestamp Error!");
+        }else{
+            if(data.length > 0){
+                callback(null);
+            }else{
+                callback(timestamp);
+            }
+        }
+    });
+}
+
 //find all the data from collection compare price
 comparePriceSchema.statics.findAllData = function (callback) {
     var data = [
-        {$match: {}},
+        {$match: {quote: "USD"}},
         {$sort: {'timestamp': 1}}
     ];
     this.aggregate(data, function(err, data){
@@ -68,18 +99,6 @@ comparePriceSchema.statics.findAllData = function (callback) {
         }
     });
 };
-
-//function to get data from URL
-function getData(url, callback){
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            callback(error, body);
-        }
-    });
-}
 
 var Compare_price = mongoose.model('Compare_Price', comparePriceSchema, 'compare_price');
 module.exports = Compare_price;
